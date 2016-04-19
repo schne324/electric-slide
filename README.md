d2a6c81f171b8e3d811f5b6ffb1e662da3fd3bbd
Harris Schneiderman <schne324@gmail.com>
2016-04-19 00:12:01 -0400

    Adds disable, highlight, and help options and updates readme accordingly

--- README.md
+++ README.md
@@ -3,12 +3,26 @@
 Accessible slider plugin
 
 ## Usage
-Called on the element which contains the handle. `jQuery('.my-container').electricSlide(options);`
+Called on the element which contains the handle.
+
+### jQuery
+
+```javascript
+jQuery(container).electricSlide(options);
+```
+
+### Contructor
+
+```javascript
+new ElectricSlide(container, options);
+```
 
 ## Options
 - `handle`: The selector for the handle element (qualified within the container). Defaults to: `'.handle'`.
 
-- `label` (optional): The selector for the label element.  An `aria-labelledby` association between the handle and the label elements will be made.
+- `label` (optional): The selector for the label element.  An `aria-labelledby` association between the handle and the label element(s) will be made.
+
+- `help` (optional): The selector for the help text.  An `aria-describedby` association between the handle and the help text element(s) will be made.
 
 - `values`: Either an array of values or an object containing the following properties:
   - `min`: the minimum value
@@ -22,7 +36,12 @@ Called on the element which contains the handle. `jQuery('.my-container').electr
     }
   ```
 - `increment` (optional): The amount to increment/decrement within the values of the slider (should really only be used with the object form of the `values` option).
+
 - `initialValue` (optional): The initial value to be set on the slider.
+
+- `highlight` (optional): Fill in the slider area up to the handle.
+
+- `disable` (optional): Disable the slider.
 
 ## Events
 - `electricSlide:init`: Fired when the slider is initialized.
@@ -55,6 +74,17 @@ Called on the element which contains the handle. `jQuery('.my-container').electr
           $(container).find('.tooltip').text($(handle).attr('aria-valuetext'))
         });
     ```
+
+## Public Methods
+- `ElectricSlide#disable`: disable the slider
+- `ElectricSlide#enable`: enable the slider
+- Example:
+
+  ```javascript
+    var container = document.querySelector('.electric-slide');
+    var slider = new ElectricSlide(container);
+    slider.disable();
+  ```
 
 ## DEV
 - `npm i && bower i`
--- build/electric-slide.js
+++ build/electric-slide.js
@@ -13,7 +13,10 @@ var defaults = {
     max: 100
   },
   initialValue: null,
-  increment: 1
+  increment: 1,
+  highlight: false,
+  disabled: false,
+  help: false
 };
 
 /**
@@ -35,6 +38,20 @@ ElectricSlide.prototype.setup = function () {
   if (options.label) {
     var $label = jQuery(options.label);
     tokenList('aria-labelledby', $handle, $label);
+  }
+
+  if (options.disabled) {
+    this.disable();
+  }
+
+  if (options.help) {
+    tokenList('aria-describedby', $handle, jQuery(options.help));
+  }
+
+  if (options.highlight) {
+    var $fill = jQuery('<span />').addClass('electric-fill');
+    $container.prepend($fill);
+    this.$fill = $fill;
   }
 
   $handle.attr({
@@ -75,6 +92,7 @@ ElectricSlide.prototype.attachEvents = function () {
     // arrow key
     .off('keydown.electricSlide')
     .on('keydown.ElectricSlide', function (e) {
+      if (that.disabled) { return; }
       var which = e.which;
       if (which >= 37 && which <= 40) {
         e.preventDefault(); // don't scroll
@@ -85,7 +103,7 @@ ElectricSlide.prototype.attachEvents = function () {
     // drag
     .off('mousedown.electricSlide')
     .on('mousedown.electricSlide', function (e) {
-      if (e.which === 3) { return; } // right click
+      if (e.which === 3 || that.disabled) { return; }
       $document // this allows for sloppy drags
         .on('mousemove.electricSlide', function (e) {
           var x = e.pageX; // TODO: for vertical sliders, use e.pageY
@@ -100,6 +118,7 @@ ElectricSlide.prototype.attachEvents = function () {
   this.$container
     // jump to
     .on('mousedown', function (e) {
+      if (e.which === 3 || that.disabled) { return; }
       if (!jQuery(e.target).is(that.$handle[0])) {
         that.onDrag(e.offsetX, true);
       }
@@ -126,9 +145,21 @@ ElectricSlide.prototype.goTo = function (value) {
   this.currentValue = value;
   this.$handle.attr('aria-valuenow', value);
   // move the slider visually to the new value
-  move(this.$handle, this.incrementWidth);
-
+  this.move();
+  // trigger change event
   this.$container.trigger('electricSlide:change', [this.$container[0], this.$handle[0]]);
+};
+
+ElectricSlide.prototype.disable = function () {
+  this.disabled = true;
+  this.$container.addClass('disabled');
+  this.$handle.attr('aria-disabled', true);
+};
+
+ElectricSlide.prototype.enable = function () {
+  this.disabled = false;
+  this.$container.removeClass('disabled');
+  this.$handle.removeAttr('aria-disabled');
 };
 
 // TODO: rename this because it is utilized for jump-to funcionality as well as drag
@@ -153,6 +184,19 @@ ElectricSlide.prototype.onDrag = function (offsetLeft, force) {
 
   this.goTo(units);
 };
+
+ElectricSlide.prototype.move = function () {
+  var $handle = this.$handle;
+  var valNow = parseInt($handle.attr('aria-valuenow'));
+  var valMin = parseInt($handle.attr('aria-valuemin'));
+  var moveTo = (valNow - valMin) * this.incrementWidth;
+  var offset = [(moveTo - ($handle.width() / 2)).toString(), 'px'].join('');
+  $handle.css('margin-left', offset);
+
+  if (this.options.highlight) {
+    this.$fill.css('width', offset);
+  }
+}
 
 /**
  * Register jQuery Plugin
@@ -206,16 +250,6 @@ function configureAriaValue($handle, options) {
   });
 
   if (isArray) { $handle.attr('aria-valuetext', vals[0]); }
-}
-
-// moves the handle
-// TODO: margin-left is presumptuous (margin-top for top-bottom vertical sliders...)
-function move($handle, incrementWidth) {
-  var halfWidth = $handle.width() / 2;
-  var valNow = parseInt($handle.attr('aria-valuenow'));
-  var valMin = parseInt($handle.attr('aria-valuemin'));
-  var moveTo = (valNow - valMin) * incrementWidth;
-  $handle.css('margin-left', [(moveTo - halfWidth).toString(), 'px'].join(''));
 }
 
 // rounds to the nearest increment
